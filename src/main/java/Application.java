c30272501df55ff7ba76b38c1c30ecf7512a9395
kuso_bs <kuso@rozetka.com.ua>
2021-03-16 21:37:53 +0200

    Initialized selenium lib

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,14 @@
 import models.Car;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import static java.lang.String.format;
 import static models.Car.count;
@@ -13,9 +18,27 @@ public class Application {
 
     private final static Logger LOG = LogManager.getLogger("Тест");
 
+
     public static void main(String[] args) {
         LOG.info("=========Start========");
+        System.setProperty("webdriver.chrome.driver", "/home/bohdan/opensource/G48Automation/src/main/resources/drivers/chrome/84.0/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
 
+        try {
+
+            driver.get("https://github.com/login");
+            driver.findElement(By.name("login")).sendKeys("BKuso");
+            driver.findElement(By.name("password")).sendKeys("123455as");
+            driver.findElement(By.name("commit")).click();
+
+            LOG.error(driver.findElement(By.xpath("//div[@class = 'flash flash-full flash-error ']/div")).getText());
+        } finally {
+            driver.quit();
+        }
+
+        /*
         String[] colors = new String[]{
                 "red",
                 "blue",
@@ -54,6 +77,8 @@ public class Application {
             LOG.error(i);
         }
         LOG.info(cars);
+
+         */
 
 
         LOG.info("=========Finish========");
